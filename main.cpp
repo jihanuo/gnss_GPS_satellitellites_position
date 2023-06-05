b5083f2ee460fd4fd2b7eb518fd12b18241b423a
吉哈诺 <flaringflame@126.com>
2023-06-06 00:51:38 +0800

    更改数据的储存方式，将其和程序放在同一个文件夹内，并采用相对路径调用数据文件，增强可用性

--- main.cpp
+++ main.cpp
@@ -2,15 +2,25 @@
 #include <iostream>
 #include "Satellites.h"
 using namespace std;
+#include <filesystem>//需使用c++17及更新标准
 
 int main()
 {
-	string path1 = "E:\\迅雷下载\\SCH200CAN_R_20231460000_01D_MN.rnx\\SCH200CAN_R_20231460000_01D_MN.rnx";	 // GPS 卫星星历在 PC 上的路径
-	string path2 = "E:\\迅雷下载\\brdc0560.22n\\results.csv";		// GPS 位置计算结果存储的路径
-	string path3 = "E:\\迅雷下载\\brdc0560.22n\\final_results.csv";
+	//string path1 = "..//资源文件//220225data.rnx";	 
+	FILE* fp;
+	std::string Data_relativePath = "\\data\\SCH200CAN_R_20231460000_01D_MN.rnx";
+	std::string currentPath = std::filesystem::current_path().string();
+
+	// 使用 C++17 的文件系统库拼接路径
+	std::string path1 = std::filesystem::path(currentPath).string() + Data_relativePath;// GPS 卫星星历在 PC 上的路径
+
+	//cout << path1 << endl;
+
+	string path2 = std::filesystem::path(currentPath).string() + "\\data\\results.csv";		// GPS 位置计算结果存储的路径
+	string path3 = std::filesystem::path(currentPath).string() + "\\data\\final_results.csv";
 	tm jiesuan_date = {};//定义解算日期
 	jiesuan_date.tm_year = 2022 - 1900;
-	jiesuan_date.tm_mon = 1;
+	jiesuan_date.tm_mon = 2;
 	jiesuan_date.tm_mday = 25;
 	jiesuan_date.tm_hour = 0;
 	jiesuan_date.tm_min = 0;
